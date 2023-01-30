d3ea5d1ede6b3596852c3d4096dead416271d8b1
Khalil Batkaev <xermed@yandex.ru>
2023-01-30 12:02:20 +0300

    [+] Решение задания 9 из презентации

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -20,8 +20,27 @@ stop = qty * STEP
 for i in range(START, stop, STEP):
     print(('*' * i).center(stop-START))
 
+print()
+
 """
 Задание №9
 Выведите в консоль таблицу умножения от 2х2 до 9х10 как на
 школьной тетрадке.
 """
+
+START = 2
+MIDDLE = 6
+STOP = 11
+TEN = 10
+
+for i in range(START, STOP):
+    for j in range(START, MIDDLE):
+        print(f'{j} * {i} = {j * i}', end='\t')
+        if j == STOP // START:
+            print()
+print()
+for i in range(START, STOP):
+    for j in range(MIDDLE, TEN):
+        print(f'{j} * {i} = {j * i}', end='\t')
+        if j == STOP - START:
+            print()
