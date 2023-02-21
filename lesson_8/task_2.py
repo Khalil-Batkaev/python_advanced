ba083da2205e04918beb5a74a3d58b7211856f2e
Khalil Batkaev <xermed@yandex.ru>
2023-02-21 10:09:44 +0300

    [+] Решения заданий с семинара

---
+++ lesson_8/task_2.py
@@ -0,0 +1,25 @@
+import json
+import os
+from pathlib import Path
+
+
+def json_gen(file: Path):
+    result = {}
+    unique_id = set()
+
+    if file.is_file() and os.path.getsize(file) > 0:
+        with open(file, 'r', encoding='utf-8') as js:
+            result = json.load(js)
+            unique_id.update(*((value.keys()) for value in result.values()))
+    while True:
+        name, user_id, lvl = input('Введите данные через пробел: ').split()
+        if not user_id in unique_id:
+            result.setdefault(int(lvl), {}).update({user_id: name})
+            with open(file, 'w', encoding='utf-8') as f:
+                print(result)
+                if user_id not in unique_id:
+                    json.dump(result, f, indent=2)
+
+
+if __name__ == '__main__':
+    json_gen(Path('files', 'names.json'))
