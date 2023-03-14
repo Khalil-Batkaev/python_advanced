2a953a8d54b2c08a35ff45d1777a3b0abbb029bf
Khalil Batkaev <xermed@yandex.ru>
2023-03-14 14:02:51 +0300

    [+] pytest date_checker

---
+++ lesson_14/home_work/dates/pytest_dates.py
@@ -0,0 +1,23 @@
+import pytest
+
+from .check_date import check_date, _is_leap_year
+
+
+def test_is_leap():
+    assert _is_leap_year(2008) == True
+    assert _is_leap_year(2000) == True
+
+
+def test_not_leap():
+    assert _is_leap_year(2001) == False
+    assert _is_leap_year(1900) == False
+
+
+def test_checker():
+    assert check_date('24.02.2023') == True
+    assert check_date('29.02.2023') == False
+    assert check_date('29.02.2020') == True
+
+
+if __name__ == '__main__':
+    pytest.main(['-v'])
